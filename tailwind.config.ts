940629828339c151d2ff1cbba8a90cd99c7a08e7
Eden Reich <eden.reich@gmail.com>
2024-03-10 16:02:02 +0100

    Add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#2A9D8F",
+        secondary: "#E9C46A",
+        accent: "#264653",
+      },
     },
   },
   plugins: [],
