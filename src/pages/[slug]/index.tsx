39cf062f6b2e3f181a72f3ee1e3066947f91efdb
Eden Reich <eden.reich@gmail.com>
2024-05-12 14:40:14 +0200

    Fix post thumbnail when posting on social media

--- src/pages/[slug]/index.tsx
+++ src/pages/[slug]/index.tsx
@@ -13,12 +13,23 @@ const Post: React.FC<PostProps> = ({ post }) => {
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>{`${post.metadata.title} | Engineering Blog`}</title>
-        <meta property="og:title" content={`${post.metadata.title} | Engineering Blog`} />
+        <meta
+          property="og:title"
+          content={`${post.metadata.title} | Engineering Blog`}
+        />
+        <meta property="og:description" content={post.metadata.excerpt} />
+        <meta
+          property="og:image"
+          content={`/img/posts/${post.metadata.thumbnail}`}
+        />
       </Head>
       <section className="px-4 sm:pl-10 sm:pr-10 max-w-4xl mx-auto">
         <h1 className="text-4xl font-bold mt-8">{post.metadata.title}</h1>
         <p className="text-gray-400 mt-2">{post.metadata.date}</p>
-        <div className="container text-xl sm:text-2xl lg:text-3xl mt-10 mb-10 break-words" dangerouslySetInnerHTML={{ __html: post.content }} />
+        <div
+          className="container text-xl sm:text-2xl lg:text-3xl mt-10 mb-10 break-words"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
       </section>
     </>
   );
